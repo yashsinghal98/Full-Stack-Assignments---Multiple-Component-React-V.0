254615d0dc7f53280217a406a7b7500c0d922b4e
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-20 13:42:00 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ class App extends Component {
 
         return(
             <>
-              <div test="project-name">TeamMeetUp
+              <div data-ns-test="project-name">TeamMeetUp
             </div>
-            <div test="project-description">The project is about giving a platform to Enterpeneurs so that they can form a team who have the required skills.The platform will give them various facilities like maintaining repositories,maintaining profile so that people could find them...etc.></div> 
+            <div data-ns-test="project-description">The project is about giving a platform to Enterpeneurs so that they can form a team who have the required skills.The platform will give them various facilities like maintaining repositories,maintaining profile so that people could find them...etc.></div> 
             </>
         )
     }
